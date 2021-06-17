3fdcfa0dc5c091bd322875f411a3e904fa1105c3
jobair00 <86056467+jobair00@users.noreply.github.com>
2021-06-17 18:53:48 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:10000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
